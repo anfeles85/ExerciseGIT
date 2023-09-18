81975727f8b87abe39839d2e1dd990d0a21eba28
Mayra Alejandra <malefrac@gmail.com>
2023-09-18 12:04:43 -0500

    Dice.py Grupo 5
    Desarrollo del Ejercicio Dice.py del grupo 5

--- ExerciseGIT/g5/Dice.py
+++ ExerciseGIT/g5/Dice.py
@@ -1,15 +1,26 @@
 """
     Fecha: 17/09/2023
-    Autor: afescobarv
+    Autor: Mayra Alejandra Franco Castaño
     Objetivo: juego del dado implementando la libreria random. El juego termina cuando el jugador saca 6 en el dado. 
     Después de cada jugada el programa debe preguntarle al jugador si quiere volver a tirar el dado. Si dice que no el juego
     termina, si dice que si se vuelve a ejecutar el dado.
-    Instrucciones: solo un miembro del equipo deberá completar el algoritmo. Después de este termine y suba los cambios
+    Instrucciones: solo un miembro del equipo deberá completar el algoritmo. Después de que este termine y suba los cambios
     a git, cada miembro restante descargará los cambios y agregará su nombre a la lista de autores. No deben hacerlo todos al 
     tiempo, sino uno por uno.
 """
 
 import random
 
-dice = random.randint(0, 6)
-print(dice)
+option = int(input("Desea volver a tirar \n1. SI: \n2. NO: "))
+
+
+while option == 1 :
+    dice = random.randint(1, 6)
+    print(dice)
+    if dice == 6 :
+        print("Fin del Juego")
+        option = 2
+    else:
+        option = int(input("Desea volver a tirar \n1. SI: \n2. NO: "))
+
+print("Fin del Juego")
