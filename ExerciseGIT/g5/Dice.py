e63518e00b5dc3218422904f5982b8d721fa3955
Nicolas Rios <nicolas6211108@gmail.com>
2023-09-18 12:10:25 -0500

    Cambio Nicolas Tapasco

--- ExerciseGIT/g5/Dice.py
+++ ExerciseGIT/g5/Dice.py
@@ -1,6 +1,6 @@
 """
     Fecha: 17/09/2023
-    Autor: Mayra Alejandra Franco Castaño
+    Autor: Mayra Alejandra Franco Castaño / Nicolas Tapasco Rios
     Objetivo: juego del dado implementando la libreria random. El juego termina cuando el jugador saca 6 en el dado. 
     Después de cada jugada el programa debe preguntarle al jugador si quiere volver a tirar el dado. Si dice que no el juego
     termina, si dice que si se vuelve a ejecutar el dado.
