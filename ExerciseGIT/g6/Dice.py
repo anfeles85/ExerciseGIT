4e3a7eccfc26440cf8b860fcd9143e395b18323d
ximena quintana <quintanaximena340@gmail.com>
2023-09-18 11:59:43 -0500

    Grupo 6
    Ejercicio grupo 6

--- ExerciseGIT/g6/Dice.py
+++ ExerciseGIT/g6/Dice.py
@@ -1,6 +1,6 @@
 """
     Fecha: 17/09/2023
-    Autor: afescobarv
+    Autor: Ximena Quintana
     Objetivo: juego del dado implementando la libreria random. El juego termina cuando el jugador saca 6 en el dado. 
     Después de cada jugada el programa debe preguntarle al jugador si quiere volver a tirar el dado. Si dice que no el juego
     termina, si dice que si se vuelve a ejecutar el dado.
@@ -11,5 +11,17 @@
 
 import random
 
-dice = random.randint(0, 6)
-print(dice)
+opcion = int(input("Desea tirar el dado: \n1.SI \n2.NO \n : "))
+
+while opcion == 1 :
+    dice = random.randint(1, 6)
+    print(dice)
+    if dice == 6:
+        print("ADIOS")
+        opcion = 2
+    else:
+        opcion = int(input("Desea tirar el dado: \n1.SI \n2.NO \n : "))
+    
+     
+print("Fin del juego")
+    
