7aac5a0434c8522bd480c54870b07036490743e5
Andres Escobar <anfelesvillada@gmail.com>
2025-03-06 09:13:58 -0500

    Commit inicial
    Commit inicial

---
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g5/Apprentice.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.exercisegit.g5;
+
+/**
+ *
+ * @author anfeles
+ */
+public class Apprentice {
+    
+}
