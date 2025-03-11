da9f1a10237659bf3f8d24410b02cf8ac655f007
Daniel Albeiro Ortiz Manzanares <danielortizmanzanares@gmail.com>
2025-03-11 09:50:08 -0500

    punto 1
    En la clase Apprentice  se crearon los atributos fullname, age y phone, con los constructores y se encapsularon. También se sobrescribio el método toString el cual retorna solamente la variable fullname.

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g4/Apprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g4/Apprentice.java
@@ -9,5 +9,45 @@ package co.edu.sena.exercisegit.g4;
  * @author anfeles
  */
 public class Apprentice {
-    
+    private String fullname;
+    private int age;
+    private int phone;
+
+    public Apprentice() {
+    }
+
+    public Apprentice(String fullname, int age, int phone) {
+        this.fullname = fullname;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Apprentice{" + "fullname=" + fullname + '}';
+    }    
 }
