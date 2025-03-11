79a32f77fcf7a68f91d355ea15a05d896fdb3924
Juan Giraldo <juanpablogiraldo367@gmail.com>
2025-03-11 09:37:14 -0500

    Cambios class apprentice
    se colocaron los atributos y los metodos a la clase apprentice

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g6/Apprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g6/Apprentice.java
@@ -9,5 +9,45 @@ package co.edu.sena.exercisegit.g6;
  * @author anfeles
  */
 public class Apprentice {
+    private String fullname;
+    private int age;
+    private int phone;
+
+    public Apprentice() {
+    }
+
+    public Apprentice(String fullname, int age, int phone) {
+        this.fullname = fullname;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
     
+    @Override
+    public String toString(){
+    return fullname;
+    }
 }
