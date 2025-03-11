715b792a50bbaf1e6f94a34a9e7e5bb847dd480d
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-03-11 09:43:17 -0500

    Create commponents
    Se crearon los constructores, getters y setters al igual que se modificó el toString.

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g3/Apprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g3/Apprentice.java
@@ -5,9 +5,53 @@
 package co.edu.sena.exercisegit.g3;
 
 /**
- *
- * @author anfeles
+ * Fecha: 11/03/2025
+ * @author 
+ * - Lina Vannesa Salcedo Cuellar
+ * - Alejandro Ocampo Saya
+ * - Juan Sebastian Rodriguez Cruz
+ * Descrippcion: Ejercicio de prueba con GIT
  */
 public class Apprentice {
-    
+    private String fullname;
+    private int age;
+    private int phone;
+
+    public Apprentice() {
+    }
+
+    public Apprentice(String fullname, int age, int phone) {
+        this.fullname = fullname;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Apprentice" + "Nombre: " + fullname;
+    }
 }
