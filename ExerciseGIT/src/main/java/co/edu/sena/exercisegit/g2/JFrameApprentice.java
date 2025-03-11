a46d801ffa96873763613b7a978c91e782c1d72a
JUAN FERNANDO VELASQUEZ <juanchibiris80@gmail.com>
2025-03-11 10:36:43 -0500

    Add combox and list
    Apprentice add three students list

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g2/JFrameApprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g2/JFrameApprentice.java
@@ -17,6 +17,18 @@ public class JFrameApprentice extends javax.swing.JFrame {
         initComponents();
     }
 
+    public JFrameApprentice(String Apprentice) {
+        this.Apprentice = Apprentice;
+    }
+
+    public String getApprentice() {
+        return Apprentice;
+    }
+
+    public void setApprentice(String Apprentice) {
+        this.Apprentice = Apprentice;
+    }
+    String Apprentice;
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,22 +38,53 @@ public class JFrameApprentice extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Aprendices");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setText("Aprendices");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(154, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(171, 171, 171))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(40, 40, 40)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(100, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -68,7 +111,16 @@ public class JFrameApprentice extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JFrameApprentice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        Apprentice[] apprentices = new Apprentice[3];
+        apprentices[0] = new Apprentice("Sebastian Rojas");
+        apprentices[1] = new Apprentice("Tatiana Carvajal");
+        apprentices[2] = new Apprentice("Fernando Velasquez");
+        
+        
+        String[] apprenticeNames = new String[apprentices.length];
+        for (int i = 0; i < apprentices.length; i++) {
+            apprenticeNames[i] = apprentices[i].getName();
+        }
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -78,5 +130,7 @@ public class JFrameApprentice extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
