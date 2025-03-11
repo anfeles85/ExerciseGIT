72d6e3ab7f5675755d28788a349d4b8d54d25826
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-03-11 09:50:20 -0500

    Atributos Aprrentice
    Se crearon los constructores y setter y el toString de la clase Aprrentice

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g2/Apprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g2/Apprentice.java
@@ -5,9 +5,57 @@
 package co.edu.sena.exercisegit.g2;
 
 /**
- *
- * @author anfeles
+ * objetivo: Un integrante, en el paquete de su grupo, deberá crear la clase Apprentice con los atributos fullname,
+ * age y phone. Crear constructores y encapsular.
+ * También deberá sobreescribir el método toString el cual retorne solamente la variable el fullname.
+ * @author grupo 2
+ * Fecha: 11/3/2025
  */
 public class Apprentice {
+    private String fullname;
+    private int age;
+    private int phone;
+
+    public Apprentice() {
+    }
+
+    public Apprentice(String fullname, int age, int phone) {
+        this.fullname = fullname;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return fullname;
+    }
     
 }
