8608a12c53997081fe9f3cf72db8ca58d2d4c624
Nicolle Melendez <nicollev2020@gmail.com>
2025-03-11 09:43:08 -0500

    Atributos principales añadidos
    Se añaden los atributos principales fullname, age y phone a la clase Aprenttice, constructores, getter and setter y se sobre escribe el .toString().

--- ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g5/Apprentice.java
+++ ExerciseGIT/src/main/java/co/edu/sena/exercisegit/g5/Apprentice.java
@@ -9,5 +9,47 @@ package co.edu.sena.exercisegit.g5;
  * @author anfeles
  */
 public class Apprentice {
+    private String fullname;
+    private int age;
+    private long phone;
+    
+    public Apprentice() {
+    }
+
+    public Apprentice(String fullname, int age, long phone) {
+        this.fullname = fullname;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Apprentice: " + "fullname: " + fullname ;
+    }
+
     
 }
